f885f368ff86a55a09fbddbe2ab340cfe0e5524b
tsalaciak <tymo4444@wp.pl>
2023-06-07 16:30:20 +0200

    07.06.23

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/extract')
+def extract():
+    return render_template("extract.html")
+
+@app.route('/product')
+def product():
+    return render_template("product.html")
+
+@app.route('/products')
+def products():
+    return render_template("products.html")
